bd22a9bda8fddcaf95adab988c940d0414e67b07
timvisee <tim@visee.me>
2021-10-08 17:54:58 +0200

    Minor tweaks

--- client/src/views/Scan.vue
+++ client/src/views/Scan.vue
@@ -214,7 +214,7 @@ export default {
   width: 100%;
   height: 100%;
 
-  background-color: rgba(0, 0, 0, .7);
+  background-color: rgba(255, 255, 255, .7);
   text-align: center;
   font-weight: bold;
   font-size: 1.4rem;
